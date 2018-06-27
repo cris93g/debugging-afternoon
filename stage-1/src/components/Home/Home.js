1733409777d06af8f24a17ed7a1be2d272726913
cris93g <37388115+cris93g@users.noreply.github.com>
2018-06-26 21:18:11 -0500

    completed

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,11 +1,12 @@
-import './Home.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import "./Home.css";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="home-landing"></div>
-        )
-    }
+  render() {
+    return <div className="home-landing" />;
+  }
 }
 
 export default Home;
